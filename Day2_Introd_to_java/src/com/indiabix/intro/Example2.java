94c7f30b65f7ccf43aecc3a08cf9f5bd912a41af
rohit <rohit@LAPTOP-LCB6VDM3>
2022-04-12 14:59:30 +0530

    First Commit

---
+++ Day2_Introd_to_java/src/com/indiabix/intro/Example2.java
@@ -0,0 +1,15 @@
+package com.indiabix.intro;
+
+import java.util.Scanner;
+
+public class Example2 {
+
+	public static void main(String[] args)
+	{
+		Scanner s=new Scanner (System.in);
+		int a=s.nextInt();
+		System.out.println(a);
+		s.close();
+	}
+
+}
