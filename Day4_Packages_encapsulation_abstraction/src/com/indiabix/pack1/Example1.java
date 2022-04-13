9abb5a262889bd8326db1f2d7c0dcf5ee6926008
rohit <rohit@LAPTOP-LCB6VDM3>
2022-04-13 15:07:48 +0530

    First Commit

---
+++ Day4_Packages_encapsulation_abstraction/src/com/indiabix/pack1/Example1.java
@@ -0,0 +1,10 @@
+package com.indiabix.pack1;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Day4_Packages_encapsulation_abstraction/src/com/indiabix/pack2/Example2.java
@@ -0,0 +1,10 @@
+package com.indiabix.pack2;
+
+public class Example2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
