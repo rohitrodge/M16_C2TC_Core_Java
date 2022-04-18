dcad3332261e5c849652f83d3559b03afdd290de
rohit <rohit@LAPTOP-LCB6VDM3>
2022-04-18 14:52:18 +0530

    First Commit

---
+++ Day6_abstract_final_super/src/com/indiabix/abstract1/Example2.java
@@ -0,0 +1,14 @@
+package com.indiabix.abstract1;
+
+public class Example2 {
+
+	public static void main(String[] args) 
+	{
+		final int a=50;
+		//a=14;
+		System.out.println(a);
+
+	}
+
+}
+
--- Day_Github/src/in/cg/git/Example1.java
+++ Day_Github/src/in/cg/git/Example1.java
@@ -1,10 +1,41 @@
-package in.cg.git;
 
-public class Example1 {
+package com.indiabix.inheritance;
 
-	public static void main(String[] args) {
+class Vehicle
+{
+	void print()
+	{
+		System.out.println("Vehicle Types : ");
+	}
+}
+
+class Car extends Vehicle
+{
+	void display()
+	{
+		System.out.println("Car :: Mercedes-Benz");
+	}
+
+}
+
+class Bike extends Vehicle
+{
+	void display1()
+	{
+		System.out.println("Bike :: R-15");
+	}
+
+}
+public class heirarchicalInheritance {
+
+	public static void main(String[] args) 
+	{
+		Bike b=new Bike();
+		Car c=new Car();
+		b.print();
+		b.display1();
+		c.display();
 		
-         System.out.println("Welcome to Rice City");
 	}
 
 }
