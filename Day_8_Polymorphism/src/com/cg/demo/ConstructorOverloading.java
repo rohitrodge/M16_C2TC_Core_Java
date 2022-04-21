54cbd7edbaf82962de453dd89a5949a774cf9c3f
rohit <rohit@LAPTOP-LCB6VDM3>
2022-04-21 13:40:14 +0530

    First Commit

---
+++ Day_8_Polymorphism/src/com/cg/demo/ConstructorOverloading.java
@@ -0,0 +1,30 @@
+package com.cg.demo;
+
+
+class Renault
+{
+	String color;
+	int speed;
+	Renault()
+	{
+		System.out.println("Renault-RXE");
+	}
+	Renault(String clr,int s)
+	{
+		color=clr;
+		speed=s;
+		System.out.println("color is "+clr+" ."+"speed is "+s+"km/hr");
+	}
+}
+public class ConstructorOverloading
+
+{
+	
+	public static void main(String[] args)
+	{
+	
+		
+
+	}
+
+}
