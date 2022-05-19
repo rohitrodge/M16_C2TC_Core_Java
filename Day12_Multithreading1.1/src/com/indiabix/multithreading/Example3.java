ff07b745fd9aebcfa46f3d70b5839dd908feeb0b
rohit <rohit@LAPTOP-LCB6VDM3>
2022-05-19 13:19:53 +0530

    first commit

---
+++ Day12_Multithreading1.1/src/com/indiabix/multithreading/Example3.java
@@ -0,0 +1,24 @@
+package com.indiabix.multithreading;
+
+
+
+public class Example3 {
+
+	public static void main(String[] args) {
+		Thread t=Thread.currentThread();
+		System.out.println("Current thread: "+t);
+		t.setName("Rohit");
+		t.setPriority(9);
+		System.out.println("Current thread: "+t);
+		try
+		{
+			Thread.sleep(1);
+		}
+		catch(InterruptedException e)
+		{
+			System.out.println(e);
+		}
+
+	}
+
+}
