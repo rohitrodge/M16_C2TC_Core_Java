98a7b3aa0ce35767c14c7a66a695c7b04e44059e
rohit <rohit@LAPTOP-LCB6VDM3>
2022-05-19 13:13:28 +0530

    first commit

---
+++ Day10_Array/src/com/indiabix/array/Example1.java
@@ -0,0 +1,10 @@
+package com.indiabix.array;
+
+public class Example1 {
+
+	public static void main(String[] args) {
+		int arr[]= {11,22,33,44,55};
+		System.out.println(arr[0]);
+	}
+
+}
---
+++ Day10_Array/src/com/indiabix/array/Example4.java
@@ -0,0 +1,29 @@
+package com.indiabix.array;
+
+class Employee
+{
+	public int ID;
+	public String emp_name;
+	Employee(int ID,String emp_name)
+	{
+		this.ID=ID;
+		this.emp_name=emp_name;
+	}
+}
+public class Example4 {
+
+	public static void main(String[] args) 
+	{
+		Employee arr[];
+		arr=new Employee[3];
+		arr[0]=new Employee(101,"Rohit");
+		arr[1]=new Employee(101,"Roshan");
+		arr[2]=new Employee(101,"Rishabh");
+		for(int i=0;i<arr.length;i++)
+		{
+			System.out.println("Element at: "+i+":"+arr[i].ID+" "+arr[i].emp_name);
+		}
+
+	}
+
+}
